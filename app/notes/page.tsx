bc623a139ad87482bb63510a7655352b79adf537
AD <154968000+dangol-anish@users.noreply.github.com>
2024-06-24 21:21:37 +0545

    added header for notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,39 @@
 import { NextPage } from "next";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { Notebook } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 interface Props {}
 
-const Page: NextPage<Props> = ({}) => {
-  return <div>Notes</div>;
+const Page: NextPage<Props> = async ({}) => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/auth/login");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="flex items-center justify-between pb-4">
+        <div className="flex flex-col items-start    items ">
+          <div className="flex items-center gap-2 ">
+            <Notebook size={28} />
+            <p className="h-[10%] text-[28px]">Notes</p>
+          </div>
+
+          <span className="text-[14px] text-slate-700 ">
+            Concise records of key information
+          </span>
+        </div>
+      </div>
+      <Separator />
+    </div>
+  );
 };
 
 export default Page;
