a8dac378296264280b650ff0d71a1e5340490db4
AD <154968000+dangol-anish@users.noreply.github.com>
2024-07-08 09:42:02 +0545

    added notes dialog with scroll content

--- app/components/notes/NoteItem.tsx
+++ app/components/notes/NoteItem.tsx
@@ -17,6 +17,18 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { deleteNotes } from "@/app/notes/actions";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface Notes {
   note_id: number;
@@ -52,41 +64,73 @@ export function NoteCard({ notes }: { notes: Notes[] }) {
         <p>There are no notes to display.</p>
       ) : (
         notes.map((item: Notes) => (
-          <Card key={item.note_id} className="flex flex-col h-full mb-5">
-            <CardHeader>
-              <CardTitle className="flex justify-between items-center">
-                <span>{item.note_title}</span>
-                <span className="text-sm font-thin">
-                  {new Date(item.inserted_at).toLocaleDateString()}
-                </span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="flex-1 overflow-y-auto">
-              <CardDescription>
-                {highlightShortener(item.note_description, 30)}
-              </CardDescription>
-            </CardContent>
-            <CardFooter className="flex justify-end items-center gap-3">
-              <span>{item.is_important ? <CircleAlert size={18} /> : ""}</span>
-              <span>{item.is_archived ? <Archive size={18} /> : ""}</span>
-              <span>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <EllipsisVertical size={18} />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="flex flex-col text-sm">
-                    <DropdownMenuItem
-                      className="hover:cursor-pointer"
-                      disabled={pending}
-                      onClick={() => handleDelete(item.note_id)}
-                    >
-                      Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </span>
-            </CardFooter>
-          </Card>
+          <Dialog key={item.note_id}>
+            <DialogTrigger asChild>
+              <Card key={item.note_id} className="flex flex-col h-full mb-5">
+                <CardHeader>
+                  <CardTitle className="flex justify-between items-center">
+                    <ScrollArea className="h-[55px] mb-5">
+                      <span className="text-sm font-light">
+                        {new Date(item.inserted_at).toLocaleDateString()}
+                      </span>
+                    </ScrollArea>
+                    <span>{item.note_title}</span>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="flex-1 overflow-y-auto">
+                  <CardDescription className="text-left">
+                    {highlightShortener(item.note_description, 30)}
+                  </CardDescription>
+                </CardContent>
+                <CardFooter className="flex justify-end items-center gap-3">
+                  <span>
+                    {item.is_important ? <CircleAlert size={18} /> : ""}
+                  </span>
+                  <span>{item.is_archived ? <Archive size={18} /> : ""}</span>
+                  <span>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <EllipsisVertical size={18} />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="flex flex-col text-sm">
+                        <DropdownMenuItem
+                          className="hover:cursor-pointer"
+                          disabled={pending}
+                          onClick={() => handleDelete(item.note_id)}
+                        >
+                          Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </span>
+                </CardFooter>
+              </Card>
+            </DialogTrigger>
+            <DialogContent className="w-[90%] md:w-full">
+              <DialogHeader>
+                <DialogTitle className="flex justify-between items-center py-4 ">
+                  {" "}
+                  <span>{item.note_title}</span>
+                  <span className="text-sm font-light">
+                    {new Date(item.inserted_at).toLocaleDateString()}
+                  </span>
+                </DialogTitle>
+                <DialogDescription>
+                  <ScrollArea className="h-[300px]">
+                    {item.note_description}
+                  </ScrollArea>
+                </DialogDescription>
+              </DialogHeader>
+
+              <DialogFooter className="justify-end">
+                <DialogClose asChild>
+                  <Button type="button" variant="outline">
+                    Close
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         ))
       )}
     </div>
